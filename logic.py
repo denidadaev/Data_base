493f3c6d577aa764a3864679eadbc7ac3464f63a
denidadaev <165592822+denidadaev@users.noreply.github.com>
2025-02-23 15:41:21 +0100

    Update logic.py

--- logic.py
+++ logic.py
@@ -9,14 +9,19 @@ data = [
     ]
 
 cur.execute("CREATE TABLE movie(?, ?, ?)", data)
+con.commit()
 
 cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+con.commit()
 
 cur.execute("SELECT score, title FROM movie ORDER BY score")
+con.commit()
 
 cur.execute("UPDATE movie SET score=10 WHERE title="Monty Python and the Holy Grail"")
+con.commit()
 
 cur.execute(" DELETE FROM movie WHERE score < 8")
+con.commit()
 
 cur.execute("DROP TABLE movies")
 con.commit()
